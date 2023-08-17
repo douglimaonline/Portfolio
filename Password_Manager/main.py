a56cfd84c34c2e912d0071d422e597bce4029bf9
Douglas <douglimaonline@gmail.com>
2023-08-17 17:00:03 -0300

    Password_Managger update

--- Password_Manager/main.py
+++ Password_Manager/main.py
@@ -48,45 +48,52 @@ window.config(pady=20)
 window.minsize(width=400, height=380)
 window.maxsize(width=400, height=380)
 
+title = LabelFrame(width=380, height=340)
+title.place(x=10, y=10)
+
 canvas = Canvas(width=200, height=200)
 padlock = PhotoImage(file="logo.png")
 canvas.create_image(100, 100, image=padlock)
-canvas.grid(column=1, row=0)
+canvas.place(x=100, y=12)
+
 
 #  Labels
-label_password = Label(text=";66r3gWaM~", padx=15, font=("arial", 20, "normal"))
-label_password.grid(column=1, row=1)
+label_password = Label(text=";7%lP[Ezm7Eu2ltN~x4SD", padx=15, font=("arial", 17, "normal"))
+label_password.place(x=12, y=210)
 
 # buttons
 copy_password = Button(text="Copiar", command=copy_password)
-copy_password.grid(column=2, row=1)
+copy_password.place(x=334, y=214)
 
 restart_password = Button(text="Gerar", command=generate_password)
-restart_password.grid(column=2, row=3)
+restart_password.place(x=338, y=244)
+
 
 # options
 var_total_char = IntVar()
-var_total_char.set(15)
-total_char = Spinbox(from_=8, to=20, width=5, textvariable=var_total_char)
-total_char.grid(column=0, row=2)
+var_total_char.set(20)
+total_char = Spinbox(from_=8, to=20, width=3, textvariable=var_total_char)
+total_char.place(x=30, y=264)
+password_lenght = Label(text="Comprimento da Senha")
+password_lenght.place(x=65, y=262)
 
 var_upper = IntVar()
 az_button = Checkbutton(text="A-Z", variable=var_upper)
 az_button.select()
-az_button.grid(column=0, row=3)
+az_button.place(x=24, y=294)
 
 var_number = IntVar()
 numbers_button = Checkbutton(text="0-9", variable=var_number)
 numbers_button.select()
-numbers_button.grid(column=0, row=4)
+numbers_button.place(x=84, y=294)
 
 var_caract = IntVar()
 charact_button = Checkbutton(variable=var_caract)
 charact_button.select()
-charact_button.grid(column=0, row=5)
+charact_button.place(x=144, y=294)
 
 charact_entry = Entry(width=30)
 charact_entry.insert(0, "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~")
-charact_entry.grid(column=1, row=5)
+charact_entry.place(x=170, y=297)
 
 window.mainloop()
